a4e5decb3946ec4cc21d43b66936b2b674975b00
Valentyn Khanmamedov <oversun@ex.ua>
2018-12-04 18:15:51 +0200

    initial 3

---
+++ timing_processor.py
@@ -0,0 +1,17 @@
+import datetime
+
+def return_dif(target_year, target_mont, target_day):
+
+    today = datetime.date.today()
+    targer = datetime.date(target_year, target_mont, target_day)
+    if targer < today:
+        output_day = today - targer
+        print('Эта дата уже наступила и прошла', output_day)
+    else:
+        output_day = targer - today
+    return output_day
+
+
+
+
+print(return_dif(2001, 12, 4))
